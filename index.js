104e8730e22619b072fc769ad30b766e11263131
pooja-786 <pjagodara13@gmail.com>
2025-01-01 13:38:44 +0530

    btn-active

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ tabs.forEach((tab, index) => {
     tab.addEventListener('click', (e) => {
         tabs.forEach(tab => { tab.classList.remove('active') });
         tab.classList.add('active');
+
+         var line = document.querySelector('.line');
+    line.style.width = e.target.offsetwidth + "px";
+        line.style.left = e.target.offsetwidth + "px";
+
+
+        all_content.forEach(content => { content.className.remove('active') });
+        all_content[index].classList.add('active')
     })
     
-    ver line = document.querySelector('.line');
-    line.style.width = e.target.offsetwidth + "px";
-    line.style.left = e.target.offsetwidth + "px";
+  
 })
 
 
