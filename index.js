cf304f8c57e46404afa6efb895ecf0838b675466
pooja-786 <pjagodara13@gmail.com>
2025-01-01 12:14:25 +0530

    js use

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const tabs = document.querySelectorAll('.tab-btn');
+const all_content = document.querySelectorAll('.content');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', (e) => {
+        tabs.forEach(tab => { tab.classList.remove('active') });
+        tab.classList.add('active');
+    })
+    
+    ver line = document.querySelector('.line');
+    line.style.width = e.target.offsetwidth + "px";
+    line.style.left = e.target.offsetwidth + "px";
+})
+
+
 
